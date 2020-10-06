9634fc17b341dabadf5c57da81c35e94ce3b619b
Shnerpy <sovietkitsunes@gmail.com>
2020-10-07 09:50:01 +1100

    1 file modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -7,47 +7,34 @@ export default function App() {
     <div>
       <Helmet>
         <title>Geroch</title>
-        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='shortcut icon' type='image/x-icon' href='/images/favicon.ico' />
       </Helmet>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <header>
+        <div class="container">
+          <img src="https://i.ibb.co/BB6sMkL/kindpng-3268817.png" alt="icon" />
+          <h1>GEROCH</h1>
+          <ul className="navigation">
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+          </ul>
+        </div>
+      </header>
+      <section>
+        <div class="container">
+          <h2>Heading Title</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante metus, bibendum sed euismod efficitur,
+            laoreet at massa. Mauris sit amet iaculis diam, eu accumsan eros. Vivamus commodo ac est a eleifend. Nunc
+            consequat tincidunt orci, at tincidunt tellus bibendum eu. Sed ac eros enim. Praesent efficitur ipsum
+            tortor, sit amet elementum lacus sollicitudin et. Sed ornare euismod ligula et venenatis.
+          </p>
+        </div>
+      </section>
+      <footer>
+        <div class="container">
+          <p>@copy 2020. All Right Reserved. Kitsun Ly.</p>
+        </div>
+      </footer>
     </div>
   );
 }
-
-/*
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" type="image/ico" href="favicon.ico">
-  </head>
-  <body>
-    <header>
-      <div class="container">
-        <h1>LOGO</h1>
-        <ul class="navigation">
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-          <li><a href="#">Link 3</a></li>
-        </ul>
-      </div>
-    </header>
-    <section>
-      <div class="container">
-        <h2>Heading Title</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante metus, bibendum sed euismod efficitur,
-          laoreet at massa. Mauris sit amet iaculis diam, eu accumsan eros. Vivamus commodo ac est a eleifend. Nunc
-          consequat tincidunt orci, at tincidunt tellus bibendum eu. Sed ac eros enim. Praesent efficitur ipsum
-          tortor, sit amet elementum lacus sollicitudin et. Sed ornare euismod ligula et venenatis.
-        </p>
-      </div>
-    </section>
-    <footer>
-      <div class="container">
-        <p>&copy 2019. All Right Reserved. Kitsun Ly.</p>
-      </div>
-    </footer>
-  </body>
-</html>
-*/
