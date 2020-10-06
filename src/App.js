96f0fa67bcf33ae811371affc4d5a55203b8314e
Shnerpy <sovietkitsunes@gmail.com>
2020-10-07 09:25:24 +1100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
+      <Helmet>
+        <title>Geroch</title>
+        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+      </Helmet>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
