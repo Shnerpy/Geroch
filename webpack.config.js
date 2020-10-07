619ad16406dcf3ead4623b5a72bbd82dcec882bd
34831479 <thomasly132@gmail.com>
2020-10-08 09:45:56 +1100

    overwritted... again

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function page(page) {`./src/components/pages/${page}/index.js`}
 
 export const entry = {
   home: page('home'),
   contact: page('contact'),
   about: page('about')
-=======
-function page(page) {`./src/components/pages/${page}/index.js`}
-
-export const entry = {
-  home: page('home'),
-  contact: page('contact'),
-  about: page('about')
->>>>>>> 7ee1a06e8b2fad6dd1caa04a11b40e5ab2533ca6
 };
