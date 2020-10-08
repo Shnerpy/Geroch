c85ed8786d7a138b9b60beab07ba177e960df670
Shnerpy <sovietkitsunes@gmail.com>
2020-10-08 14:59:38 +1100

    1 file modified

--- src/components/pages/Magic.js
+++ src/components/pages/Magic.js
@@ -2,13 +2,11 @@ import React from "react";
 import {Helmet} from "react-helmet";
 
 export default function Magic() {
-  return (
-    <div>
-      <Helmet>
-        <script>
-          window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-        </script>
-      </Helmet>
-    </div>
-  );
+  return (<>
+    <Helmet>
+      <script>
+        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+      </script>
+    </Helmet>
+  </>);
 }
